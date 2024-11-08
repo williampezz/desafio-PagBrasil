1898b6de358157e3c3f82eea12ae03eead64f6e2
william <williamaugustopez@gmail.com>
2024-11-08 18:57:30 -0300

    add readme

--- cypress/e2e/backend/formSubmit.cy.js
+++ cypress/e2e/backend/formSubmit.cy.js
@@ -2,9 +2,9 @@ describe('Backend - Teste de validação do campo Name', () => {
     it('Deve retornar sucesso quando o campo Name for válido', () => {
         cy.request({
             method: 'POST',
-            url: '/api/form-submit',  
+            url: '/api/form-submit',
             body: {
-                name: 'William Pezzi',  
+                name: 'William Pez',
                 email: 'william@teste.com',
                 company: 'Pag Brasil',
                 website: 'https://www.pagbrasil.com/pt-br/',
@@ -12,8 +12,8 @@ describe('Backend - Teste de validação do campo Name', () => {
                 inquiry: 'Teste de formulário'
             }
         }).then((response) => {
-            expect(response.status).to.eq(200);  
-            expect(response.body.sucesso).to.be.true;  
+            expect(response.status).to.eq(200);
+            expect(response.body.sucesso).to.be.true;
         });
     });
 });
@@ -23,20 +23,20 @@ describe('Backend - Teste de erro no campo Name', () => {
     it('Deve retornar erro quando o campo Name for inválido', () => {
         cy.request({
             method: 'POST',
-            url: '/api/form-submit',  
+            url: '/api/form-submit',
             body: {
-                name: '', 
+                name: '',
                 email: 'william@teste.com',
                 company: 'Pag Brasil',
                 website: 'https://www.pagbrasil.com/pt-br/',
                 phone: '(51) 99999-9942',
                 inquiry: 'Teste de formulário'
             },
-            failOnStatusCode: false  
+            failOnStatusCode: false
         }).then((response) => {
-            expect(response.status).to.eq(412);  
-            expect(response.body.sucesso).to.be.false;  
-            expect(response.body.erro).to.eq('Campo Name inválido.');  
+            expect(response.status).to.eq(412);
+            expect(response.body.sucesso).to.be.false;
+            expect(response.body.erro).to.eq('Campo Name inválido.');
         });
     });
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,44 +8,41 @@ describe('Acessar formulario', () => {
     cy.visit('https://www.pagbrasil.com/fb831484f9611648f05411b3746466b6/form.html')
   })
 
-  it('Envio sucedido do formulário', ()=> {
+  it('Envio sucedido do formulário', () => {
     Form.validateSendForm()
 
   })
 
-  it('Enviar formulario com campo Name em branco', ()=> {
+  it('Enviar formulario com campo Name em branco', () => {
     Form.validateFormNameNull()
 
   })
 
-  it('Enviar formulario com campo Email em branco', ()=> {
+  it('Enviar formulario com campo Email em branco', () => {
     Form.validateFormEmailNull()
 
   })
 
-  it('Enviar formulario com campo Email invalido', ()=> {
+  it('Enviar formulario com campo Email invalido', () => {
     Form.validateFormInvalidEmail()
 
   })
 
-  it('Enviar formulario com campo Company em branco', ()=> {
+  it('Enviar formulario com campo Company em branco', () => {
     Form.validateFormCompanyNull()
 
   })
 
-  it('Enviar formulario com campo WebSite em branco', ()=> {
+  it('Enviar formulario com campo WebSite em branco', () => {
     Form.validateFormWebsiteNull()
 
   })
 
-  it('Enviar formulario com campo website invalido', ()=> {
+  it('Enviar formulario com campo website invalido', () => {
     Form.validateFormWebSiteInvalid()
 
   })
 
-
-  
-  
 
 
 })
